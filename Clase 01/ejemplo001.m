699ae6edca1aa7276fbb933ff1594abebdfc91bb
jmello <julio_mello@hacienda.gov.py>
2024-02-24 15:02:08 -0300

    Se crea el ejemplo 001

---
+++ Clase 01/ejemplo001.m
@@ -0,0 +1,16 @@
+close all   % Cierra todas las ventanas
+clc         % Limpia la terminal
+
+msj = 'Bienvenidos a Matlab\n';
+fprintf(msj);
+
+var = 6.23;
+fprintf('El valor de var = %f \n',var);
+
+x = input('Ingresar un valor: ');
+txt = input('Escribir un mensaje: ',"s");
+
+% Grafica una funcion cuadratica
+x = [-20:20];
+y = -x.^2+5*x-21;
+plot(x,y)
