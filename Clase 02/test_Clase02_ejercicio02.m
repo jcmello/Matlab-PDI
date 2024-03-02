8d53cc988062eab606f44fb69872177e8ca00eb0
Alexander Rivas <alexrivasbenitez324@gmail.com>
2024-03-02 15:31:40 -0300

    Se agrega el ejercicio 02

---
+++ Clase 02/test_Clase02_ejercicio02.m
@@ -0,0 +1,11 @@
+% Programa que recibe dos números, reste y muestre el resultado.
+
+% Ingresamos los dos numeros
+
+x = input('Ingrese un valor: ');
+
+y = input('Ingrese otro valor: ');
+
+z = restarDos(x,y);
+
+fprintf('El resultado de la resta es: %d', z);
