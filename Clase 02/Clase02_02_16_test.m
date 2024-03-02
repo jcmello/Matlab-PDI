bffb8822b0f7a5db095b90f18266ab3a17e02361
jmello <julio_mello@hacienda.gov.py>
2024-03-02 14:54:42 -0300

    Se agrega ejercicio 14

---
+++ Clase 02/Clase02_02_16_test.m
@@ -0,0 +1,10 @@
+% Escribir un programa que reciba una matriz y determine su media aritmética (promedio)
+
+clear all
+close all
+clc
+
+% 
+a = [1 2 3; 4 5 6; 7 8 9];
+b = mediaM(a);
+fprintf('La media es: %4.1f\n',b)
