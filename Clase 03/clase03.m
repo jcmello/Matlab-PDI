88410bdead1405ce3cc1856606d2b7c8c64a0361
jmello <julio_mello@hacienda.gov.py>
2024-03-16 14:43:34 -0300

    Se agrega imagen a color

--- Clase 03/clase03.m
+++ Clase 03/clase03.m
@@ -14,8 +14,19 @@ disp(N)
 whos f
 
 %Modificar la imagen
-g = f+128;
+g = f+50;
 figure, imshow(g)
 
 %Guardar la nueva imagen
 imwrite(g,'einstein-low-contrast-128.tif');
+
+%Mostrar informacion de einstein-low-contrast.tif
+K = imfinfo('einstein-low-contrast.tif');
+image_bytes=K.Width*K.Height*K.BitDepth/8;
+compresion_bytes=K.FileSize;
+compresion_tasa=image_bytes/compresion_bytes;
+
+%Imagen a color
+h = imread('lenna-color.tif');
+figure, imshow(h)
+[F,C,P]=size(h);
